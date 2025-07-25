4eddca9b72e4635766d20035ca9e2a8bc4059cd7
Khai Duong <duongkhai@gmail.com>
2025-07-25 13:24:08 +0200

    [ENH] Create minimal setup.py

---
+++ monksystem/monksystem/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MONK-system',
+    version='1.0.0',
+    url='https://github.com/OUH-MESHLab/MONK-system',
+    author='Oslo University Hospital',
+    author_email='author@test.com',
+    description='Description of my package',
+    packages=find_packages(),
+)
